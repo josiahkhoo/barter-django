97be99f2cc6d145f3e55e8a92ef8b8815fb8cccd
josiahkhoo <josiahkhooshaoqi@gmail.com>
2020-07-26 21:24:44 +0800

    added routes for equipment view

--- characters/models.py
+++ characters/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.postgres.fields import JSONField, ArrayField
 from django.utils import timezone
 from users.models import User
-
+from equipments.models import Equipment
+from equipments.utils import *
 # Create your models here.
 
 
@@ -13,3 +14,27 @@ class Character(models.Model):
         User, on_delete=models.CASCADE, related_name='characters')
     datetime_added = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
+    currently_equipped = "False"
+
+    def equip(self, equipment):
+        battles = self.battles.all()
+        equipments = list(
+            filter(lambda x: x, map(lambda x: x.equipment, battles)))
+        if equipment not in equipments:
+            raise Exception("Equipment not in inventory")
+        if equipment.equipment_type == int(EquipmentType.HELMET):
+            self.appearance_config["ValueList"][5] = \
+                equipment.appearance_config["ValueList"][5]
+        elif equipment.equipment_type == int(EquipmentType.CHEST):
+            self.appearance_config["ValueList"][8] = \
+                equipment.appearance_config["ValueList"][8]
+        elif equipment.equipment_type == int(EquipmentType.SHIELD):
+            self.appearance_config["ValueList"][13] = \
+                equipment.appearance_config["ValueList"][13]
+        elif equipment.equipment_type == int(EquipmentType.WEAPON):
+            self.appearance_config["ValueList"][9] = \
+                equipment.appearance_config["ValueList"][9]
+        else:
+            raise Exception("Can't equip specified item")
+        self.save()
+        return self
--- equipments/views.py
+++ equipments/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from barter.utils import serializer_to_body, post_request_parser, serializer_to_many_body
 from .serializers import EquipmentSerializer
 from .models import Equipment
+from characters.serializers import *
 
 # Create your views here.
 
@@ -30,3 +31,18 @@ class EquipmentView(APIView):
             )
             return Response(body, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, pk):
+        """
+        Retrieves character id from form
+        """
+        user = request.user
+        equipment = get_object_or_404(Equipment, pk=pk)
+        data = post_request_parser(request)
+        character_id = data.get('character_id', data)
+        character = user.characters.get(pk=character_id)
+        character = character.equip(equipment)
+        body = serializer_to_body(
+            CharacterSerializer, character, "character"
+        )
+        return Response(body, status=status.HTTP_200_OK)
